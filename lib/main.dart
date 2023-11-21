3cb62306687f49a08767ef91121e006e106b5f3e
ekyrizky <m.rizkyananda57@gmail.com>
2023-11-21 15:32:27 +0700

    add fonts: farro

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:ekyrizky_portfolio/utils/constants.dart';
 import 'package:ekyrizky_portfolio/utils/screen_helper.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,6 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: kPrimaryColor,
         canvasColor: kBackgroundColor,
+        textTheme: GoogleFonts.farroTextTheme(),
       ),
       home: ScreenHelper(
         desktop: Container(
