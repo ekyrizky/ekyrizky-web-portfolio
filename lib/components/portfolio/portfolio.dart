96a14d223df44aa5511f098b99288884d1614ca6
ekyrizky <muhammad.ananda@visionplus.id>
2023-12-07 15:16:21 +0700

    update portfolio items

--- lib/components/portfolio/portfolio.dart
+++ lib/components/portfolio/portfolio.dart
@@ -1,4 +1,4 @@
-import 'package:ekyrizky_portfolio/components/portfolio/portfolio_desc.dart';
+import 'package:ekyrizky_portfolio/components/portfolio/portfolio_items.dart';
 import 'package:ekyrizky_portfolio/models/portfolio_model.dart';
 import 'package:ekyrizky_portfolio/utils/screen_helper.dart';
 import 'package:flutter/material.dart';
@@ -35,7 +35,7 @@ List<PortfolioModel> portfolioItems = [
           "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
       image: 'assets/portfolio_haibunda.png'),
   PortfolioModel(
-      title: "insertlive",
+      title: "Insertlive",
       category: "Android App",
       description:
           "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
@@ -53,62 +53,17 @@ class Portfolio extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    double paddingHorizontal = MediaQuery.of(context).size.width /
+    double padding = MediaQuery.of(context).size.width /
         2 *
-        (ScreenHelper.isTablet(context) ? .15 : .30);
-    return ScreenHelper(
-      desktop: _build(paddingHorizontal),
-      tablet: _build(paddingHorizontal),
-      mobile: _buildMobile(),
-    );
-  }
+        ScreenHelper.getPadding(context);
 
-  Widget _build(double padding) {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: padding),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Image.asset(
-                'assets/portfolio_vision.png',
-                width: 200.0,
-                fit: BoxFit.cover,
-              ),
-              SizedBox(
-                width: padding,
-              ),
-              const Expanded(
-                child: PortfolioDesc(),
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildMobile() {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 52),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Image.asset(
-            'assets/portfolio_vision.png',
-            width: 200.0,
-            fit: BoxFit.fill,
-          ),
-          const SizedBox(
-            height: 16,
-          ),
-          const PortfolioDesc(),
-        ],
+        children:
+            portfolioItems.map((item) => PortfolioItems(data: item)).toList(),
       ),
     );
   }
