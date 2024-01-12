6bdd6ce5ee8387c0a78f85827c3e4cee44c68468
ekyrizky <m.rizkyananda57@gmail.com>
2024-01-12 11:14:54 +0700

    add social icon

--- lib/pages/home_revamp.dart
+++ lib/pages/home_revamp.dart
@@ -1,4 +1,5 @@
 import 'package:ekyrizky_portfolio/components/organism/header_organism.dart';
+import 'package:ekyrizky_portfolio/utils/constants.dart';
 import 'package:flutter/material.dart';
 
 class HomeRevamp extends StatelessWidget {
@@ -6,6 +7,15 @@ class HomeRevamp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const HeaderOrganism();
+    return Scaffold(
+      backgroundColor: kBackgroundColor,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            HeaderOrganism(),
+          ],
+        ),
+      ),
+    );
   }
 }
